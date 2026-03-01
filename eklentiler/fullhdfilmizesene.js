4495193c0259b22943db955ba30e2b15c94ca7c5
mooncrown04 <mooncrown04@gmail.com>
2026-03-02 01:10:37 +0300

    Update fullhdfilmizesene.js

--- eklentiler/fullhdfilmizesene.js
+++ eklentiler/fullhdfilmizesene.js
@@ -1,17 +1,16 @@
 // ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
-
 var BASE_URL = 'https://www.fullhdfilmizlesene.live';
 
+// Player'ın videoyu "yasal" görmesi için gereken başlıklar
 var STREAM_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'Origin': BASE_URL,
-    'Referer': BASE_URL + '/',
     'Sec-Fetch-Dest': 'video',
     'Sec-Fetch-Mode': 'no-cors',
     'Sec-Fetch-Site': 'cross-site'
 };
 
-// ==================== KRİTİK ÇÖZÜCÜLER ====================
+// ==================== KRİTİK TEMİZLİK VE DECODE ====================
 
 function rot13(str) {
     if (!str) return null;
@@ -20,23 +19,22 @@ function rot13(str) {
     });
 }
 
-// 1004 Malformed URL hatasını önlemek için URL temizleyici
-function cleanUrl(url) {
+// 1004 Hatasını bitiren temizlik fonksiyonu
+function fixUrl(url) {
     if (!url) return null;
-    // Çift ters bölüleri, tırnakları ve gereksiz boşlukları temizle
-    var cleaned = url.replace(/\\/g, '').replace(/"/g, '').replace(/'/g, '').trim();
+    var cleaned = url.replace(/\\/g, '').replace(/"/g, "").replace(/'/g, "").replace(/\s/g, "");
     if (cleaned.startsWith('//')) cleaned = 'https:' + cleaned;
     return cleaned;
 }
 
-function hexToString(hex) {
+function hexToUrl(hex) {
     try {
-        var cleanHex = hex.replace(/\\\\x/g, '').replace(/\\x/g, '').replace(/[^a-fA-F0-9]/g, '');
+        var raw = hex.replace(/\\\\x/g, '').replace(/\\x/g, '');
         var str = '';
-        for (var i = 0; i < cleanHex.length; i += 2) {
-            str += String.fromCharCode(parseInt(cleanHex.substr(i, 2), 16));
+        for (var i = 0; i < raw.length; i += 2) {
+            str += String.fromCharCode(parseInt(raw.substr(i, 2), 16));
         }
-        return cleanUrl(str); // Çıkan sonucu hemen temizle
+        return fixUrl(str);
     } catch (e) { return null; }
 }
 
@@ -49,13 +47,12 @@ function getStreams(tmdbId, mediaType) {
         var tmdbUrl = 'https://api.themoviedb.org/3/movie/' + tmdbId + '?language=tr-TR&api_key=4ef0d7355d9ffb5151e987764708ce96';
 
         fetch(tmdbUrl).then(function(res) { return res.json(); }).then(function(data) {
-            var searchUrl = BASE_URL + '/arama/' + encodeURIComponent(data.title);
-            return fetch(searchUrl);
+            return fetch(BASE_URL + '/arama/' + encodeURIComponent(data.title));
         }).then(function(res) { return res.text(); }).then(function(searchHtml) {
             var filmMatch = searchHtml.match(/<li[^>]*class=["']film["'][^>]*>[\s\S]*?<a[^>]+href=["']([^"']+)["']/i);
             if (!filmMatch) return resolve([]);
             
-            var filmUrl = filmMatch[1].startsWith('http') ? filmMatch[1] : BASE_URL + filmMatch[1];
+            var filmUrl = filmMatch[1].indexOf('http') === -1 ? BASE_URL + filmMatch[1] : filmMatch[1];
             return fetch(filmUrl).then(function(res) { 
                 return res.text().then(function(html) { return { html: html, url: filmUrl }; });
             });
@@ -75,14 +72,14 @@ function getStreams(tmdbId, mediaType) {
                 rawLinks.forEach(function(enc, index) {
                     var decoded = atob(rot13(enc));
                     
-                    // IFRAME/EXTRACTOR İŞLEME (Rapid, Atom, VidMoxy)
-                    if (decoded.indexOf('http') !== -1 && (decoded.indexOf('rapidvid') !== -1 || decoded.indexOf('vidmoxy') !== -1 || decoded.indexOf('atom') !== -1)) {
+                    // IFRAME/EXTRACTOR (RapidVid, Atom, VidMoxy)
+                    if (decoded && (decoded.indexOf('rapidvid') !== -1 || decoded.indexOf('vidmoxy') !== -1 || decoded.indexOf('atom') !== -1)) {
                         var p = fetch(decoded, { headers: { 'Referer': obj.url } })
                             .then(function(r) { return r.text(); })
                             .then(function(fHtml) {
                                 var fileMatch = fHtml.match(/file["']:\s*["']([^"']+)["']/);
                                 if (fileMatch) {
-                                    var finalUrl = hexToString(fileMatch[1]);
+                                    var finalUrl = hexToUrl(fileMatch[1]);
                                     if (finalUrl && finalUrl.indexOf('http') !== -1) {
                                         return {
                                             name: '⌜ FHD ⌟ ' + key.toUpperCase() + ' #' + (index + 1),
@@ -96,18 +93,19 @@ function getStreams(tmdbId, mediaType) {
                             }).catch(function() { return null; });
                         streamPromises.push(p);
                     } else if (decoded && decoded.indexOf('http') !== -1) {
+                        var cleanedDirect = fixUrl(decoded);
                         streamPromises.push(Promise.resolve({
                             name: '⌜ FHD ⌟ ' + key.toUpperCase() + ' #' + (index + 1),
-                            url: cleanUrl(decoded),
-                            type: decoded.indexOf('.m3u8') !== -1 ? 'M3U8' : 'VIDEO',
+                            url: cleanedDirect,
+                            type: cleanedDirect.indexOf('.m3u8') !== -1 ? 'M3U8' : 'VIDEO',
                             headers: Object.assign({}, STREAM_HEADERS, { 'Referer': obj.url })
                         }));
                     }
                 });
             });
 
-            Promise.all(streamPromises).then(function(results) {
-                resolve(results.filter(function(x) { return x !== null; }));
+            Promise.all(streamPromises).then(function(res) {
+                resolve(res.filter(function(x) { return x !== null; }));
             });
         }).catch(function() { resolve([]); });
     });
