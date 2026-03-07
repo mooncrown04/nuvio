ce357dd1e8cbc2528238f3806adf95c1ff4ac56f
mooncrown04 <mooncrown04@gmail.com>
2026-03-07 05:09:51 +0300

    Update diziyou.js

--- eklentiler/diziyou.js
+++ eklentiler/diziyou.js
@@ -1,23 +1,13 @@
-console.log('[DZB-LOG] >>> TEST BASLADI <<<');
+// [DZB-LOG] Test baslatildi
+console.log('[DZB-LOG] SADECE FONKSIYON TESTI');
 
-// 1. Format: Global Değişken (En yaygın olanı)
-var scraper = {
-    search: function(query) {
-        console.log('[DZB-LOG] Arama tetiklendi: ' + query);
-        return [];
-    }
-};
-
-// 2. Format: Fonksiyonun doğrudan kendisi
-function search(query) {
-    console.log('[DZB-LOG] Direkt fonksiyon tetiklendi: ' + query);
+async function search(query) {
+    console.log('[DZB-LOG] ARAMA YAPILIYOR: ' + query);
     return [];
 }
 
-// 3. Format: QuickJS için nesneyi açıkta bırakma
-scraper;
-
-// 4. Format: Bazı sistemler için 'module.exports'
-try {
-    module.exports = scraper;
-} catch(e) {}
+// Bazı sistemler nesneyi en altta çıplak görmek ister
+({
+    name: "DiziYou",
+    search: search
+});
