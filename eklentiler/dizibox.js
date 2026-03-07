90715293c052dc10d769ae2d3ff1cebfb4f5721e
mooncrown04 <mooncrown04@gmail.com>
2026-03-07 04:04:39 +0300

    Update dizibox.js

--- eklentiler/dizibox.js
+++ eklentiler/dizibox.js
@@ -1,67 +1,61 @@
 var BASE_URL = 'https://www.dizibox.live';
 
-// Amazon/FireTV cihazları için timeout kontrolü eklenmiş fetch
-function fetchWithTimeout(url, options, timeout = 7000) {
-    return Promise.race([
-        fetch(url, options),
-        new Promise((_, reject) => setTimeout(() => reject(new Error('timeout')), timeout))
-    ]);
-}
-
 function getStreams(tmdbId, mediaType, seasonNum, episodeNum) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function(resolve) {
         if (mediaType !== 'tv') return resolve([]);
 
-        // 1. ADIM: TMDB verisini beklemeden doğrudan arama yapmaya çalışıyoruz
-        // Bu, loglardaki timeout ve certificate trust hatalarını azaltır.
+        // Sertifika hatalarını ve yavaşlığı aşmak için TMDB sorgusunu çok hızlı tutuyoruz
         var tmdbUrl = 'https://api.themoviedb.org/3/tv/' + tmdbId + '?api_key=4ef0d7355d9ffb5151e987764708ce96';
 
-        fetchWithTimeout(tmdbUrl, {})
-            .then(function(res) { return res.json(); })
+        fetch(tmdbUrl)
+            .then(function(res) { 
+                if(!res.ok) throw new Error('tmdb_fail');
+                return res.json(); 
+            })
             .then(function(data) {
                 var query = data.name || data.original_name;
-                // Arama URL'sini oluştur
                 var searchUrl = BASE_URL + '/?s=' + encodeURIComponent(query);
                 
-                return fetchWithTimeout(searchUrl, {
-                    headers: { 'User-Agent': 'Mozilla/5.0' }
+                // Cihazın "trust" sürecini hızlandırmak için sadece gerekli headerlar
+                return fetch(searchUrl, { 
+                    headers: { 'User-Agent': 'Mozilla/5.0' },
+                    timeout: 5000 
                 });
             })
             .then(function(res) { return res.text(); })
             .then(function(html) {
-                // Saf Regex: En hızlı sonuç ayıklama yöntemi
-                var linkMatch = html.match(/href="(https:\/\/www\.dizibox\.live\/dizi\/[^"]+)"/);
+                // Regex ile dizi slug'ını bul
+                var linkMatch = html.match(/href="https:\/\/www\.dizibox\.live\/dizi\/([^/"]+)/);
                 if (!linkMatch) return resolve([]);
 
-                var slug = linkMatch[1].split('/dizi/')[1].replace(/\//g, '');
+                var slug = linkMatch[1];
                 var targetUrl = BASE_URL + '/' + slug + '-' + seasonNum + '-sezon-' + episodeNum + '-bolum-hd-1-izle/';
 
-                return fetchWithTimeout(targetUrl, {
-                    headers: { 'Referer': BASE_URL }
+                return fetch(targetUrl, { 
+                    headers: { 'Referer': BASE_URL },
+                    timeout: 5000
                 });
             })
             .then(function(res) { return res.text(); })
             .then(function(html) {
-                // Iframe'i hızlıca bul
+                // Iframe yakala
                 var iframeMatch = html.match(/<iframe[^>]+src="([^"]+)"/i);
-                
                 if (iframeMatch) {
-                    var finalUrl = iframeMatch[1];
-                    if (finalUrl.startsWith('//')) finalUrl = 'https:' + finalUrl;
-
+                    var src = iframeMatch[1];
+                    if (src.startsWith('//')) src = 'https:' + src;
+                    
                     resolve([{
                         name: "DiziBox",
-                        url: finalUrl,
+                        url: src,
                         quality: "1080p",
-                        headers: { 'Referer': BASE_URL, 'Origin': BASE_URL },
-                        provider: "dizibox"
+                        headers: { 'Referer': BASE_URL }
                     }]);
                 } else {
                     resolve([]);
                 }
             })
-            .catch(function(err) {
-                console.log('[DiziBox Error]', err.message);
+            .catch(function() {
+                // Herhangi bir hatada (timeout, cert error) boş dön ki sistem kilitlenmesin
                 resolve([]);
             });
     });
