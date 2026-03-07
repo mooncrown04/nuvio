5e2b3f366dd6c1604a2ba7e0dd804f71ff352a5e
mooncrown04 <mooncrown04@gmail.com>
2026-03-07 15:47:53 +0300

    Update dizibox.js

--- eklentiler/dizibox.js
+++ eklentiler/dizibox.js
@@ -1,48 +1,55 @@
-var VER = '1.2.1-PC_EMU';
+var VER = '1.2.5-EMU_FIX';
 
-// Sistem yüklenir yüklenmez bir işaret bırakalım
-console.log('[Dizibox V' + VER + '] DOSYA SİSTEME YÜKLENDİ');
+// DİKKAT: Bu log dosya yüklendiği an görünmeli!
+console.log('--- [Dizibox V' + VER + '] DOSYA AKTİF EDİLDİ ---');
 
 var TMDB_KEY = '4ef0d7355d9ffb5151e987764708ce96';
 
+function findFirst(html, pattern) {
+    var regex = new RegExp(pattern, 'i');
+    var match = regex.exec(html);
+    return match ? match : null;
+}
+
 async function getStreams(tmdbId, mediaType, seasonNum, episodeNum) {
-    console.log('[Dizibox V' + VER + '] getStreams tetiklendi! ID: ' + tmdbId);
-    
+    // Fonksiyonun içine girdiğimizi teyit edelim
+    console.log('[Dizibox V' + VER + '] getStreams ÇALIŞTI -> ID: ' + tmdbId);
+
+    if (mediaType !== 'tv') return [];
+
     try {
-        if (mediaType !== 'tv') return [];
+        // 1. TMDB İsteği (Sertifika hatası ihtimaline karşı try-catch içinde)
+        console.log('[Dizibox V' + VER + '] TMDB Sorgulanıyor...');
+        const tmdbRes = await fetch('https://api.themoviedb.org/3/tv/' + tmdbId + '?api_key=' + TMDB_KEY + '&language=tr-TR');
+        const tmdbData = await tmdbRes.json();
+        const query = tmdbData.name || tmdbData.original_name;
+        
+        console.log('[Dizibox V' + VER + '] Dizi Adı: ' + query);
 
-        // Adım 1: TMDB Testi
-        const tmdbUrl = 'https://api.themoviedb.org/3/tv/' + tmdbId + '?api_key=' + TMDB_KEY + '&language=tr-TR';
-        console.log('[Dizibox V' + VER + '] TMDB isteği gidiyor...');
+        // 2. Dizibox Arama (HTTP Kullanıyoruz - Saat hatasından etkilenmemek için)
+        var BASE_URL = 'http://www.dizibox.pw'; 
+        const searchUrl = BASE_URL + '/?s=' + encodeURIComponent(query);
         
-        const res = await fetch(tmdbUrl);
-        const data = await res.json();
-        const query = data.name || data.original_name;
-        
-        console.log('[Dizibox V' + VER + '] Dizi adı yakalandı: ' + query);
-
-        // Adım 2: Arama Testi (PC'de bazen .pw açılmazsa .live dene)
-        const searchUrl = 'https://www.dizibox.pw/?s=' + encodeURIComponent(query);
-        console.log('[Dizibox V' + VER + '] Siteye bağlanılıyor: ' + searchUrl);
-        
+        console.log('[Dizibox V' + VER + '] Arama Sayfası: ' + searchUrl);
         const sRes = await fetch(searchUrl);
         const html = await sRes.text();
-        
-        // Link yakalama (En basit Regex)
-        const linkMatch = html.match(/href="(https?:\/\/www\.dizibox\.pw\/[^"]+)"[^>]*rel="bookmark"/i);
+
+        // 3. Link Ayıklama
+        const linkMatch = findFirst(html, 'href="(https?:\\/\\/[^"]+dizibox[^"]+)"[^>]*rel="bookmark"');
         
         if (!linkMatch) {
-            console.log('[Dizibox V' + VER + '] HATA: Arama sonuç sayfasında link bulunamadı.');
+            console.log('[Dizibox V' + VER + '] HATA: Arama sonucunda link yakalanamadı.');
             return [];
         }
 
-        const epUrl = linkMatch[1].replace(/\/$/, '') + '-sezon-' + seasonNum + '-bolum-' + episodeNum + '-izle/';
-        console.log('[Dizibox V' + VER + '] Hedef Bölüm: ' + epUrl);
+        const epUrl = linkMatch[1].replace('https:', 'http:') + '-sezon-' + seasonNum + '-bolum-' + episodeNum + '-izle/';
+        console.log('[Dizibox V' + VER + '] Son Durak: ' + epUrl);
 
         return [{
-            name: '⌜ Dizibox V' + VER + ' ⌟',
-            url: epUrl, // Test amaçlı sayfa URL'sini dönüyoruz
-            quality: '1080p'
+            name: "⌜ Dizibox V" + VER + " ⌟",
+            url: epUrl,
+            quality: "1080p",
+            headers: { 'Referer': BASE_URL }
         }];
 
     } catch (e) {
@@ -51,9 +58,9 @@ async function getStreams(tmdbId, mediaType, seasonNum, episodeNum) {
     }
 }
 
-// Global Export
+// Nuvio/Mooncrown için tam uyumlu export
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { getStreams };
+    module.exports = { getStreams: getStreams };
 } else {
     global.getStreams = getStreams;
 }
