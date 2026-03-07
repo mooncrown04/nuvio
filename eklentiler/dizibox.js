ca7e1fbf9cfdc242d8daf30bd23df59b9f9623aa
mooncrown04 <mooncrown04@gmail.com>
2026-03-07 15:52:59 +0300

    Update dizibox.js

--- eklentiler/dizibox.js
+++ eklentiler/dizibox.js
@@ -1,7 +1,6 @@
-var VER = '1.2.5-EMU_FIX';
-
-// DİKKAT: Bu log dosya yüklendiği an görünmeli!
-console.log('--- [Dizibox V' + VER + '] DOSYA AKTİF EDİLDİ ---');
+var VER = '1.3.0'; 
+// Saat farkı olsa bile dosyanın yüklendiğini görmek için düz log
+console.log('>>> [Dizibox V' + VER + '] KOD YÜKLENDİ - TEST BAŞLIYOR <<<');
 
 var TMDB_KEY = '4ef0d7355d9ffb5151e987764708ce96';
 
@@ -12,53 +11,60 @@ function findFirst(html, pattern) {
 }
 
 async function getStreams(tmdbId, mediaType, seasonNum, episodeNum) {
-    // Fonksiyonun içine girdiğimizi teyit edelim
-    console.log('[Dizibox V' + VER + '] getStreams ÇALIŞTI -> ID: ' + tmdbId);
+    console.log('[Dizibox V' + VER + '] Sorgu Tetiklendi. TMDB ID: ' + tmdbId);
 
     if (mediaType !== 'tv') return [];
 
     try {
-        // 1. TMDB İsteği (Sertifika hatası ihtimaline karşı try-catch içinde)
-        console.log('[Dizibox V' + VER + '] TMDB Sorgulanıyor...');
+        // SSL/Saat hatasını aşmak için TMDB isteğini try-catch içine alıyoruz
+        console.log('[Dizibox V' + VER + '] 1. TMDB Bilgisi Çekiliyor...');
         const tmdbRes = await fetch('https://api.themoviedb.org/3/tv/' + tmdbId + '?api_key=' + TMDB_KEY + '&language=tr-TR');
         const tmdbData = await tmdbRes.json();
         const query = tmdbData.name || tmdbData.original_name;
         
-        console.log('[Dizibox V' + VER + '] Dizi Adı: ' + query);
+        console.log('[Dizibox V' + VER + '] 2. Dizi Adı Bulundu: ' + query);
 
-        // 2. Dizibox Arama (HTTP Kullanıyoruz - Saat hatasından etkilenmemek için)
+        // PC Emülatörü için HTTP kullanımı (Sertifika hatasını bypass eder)
         var BASE_URL = 'http://www.dizibox.pw'; 
         const searchUrl = BASE_URL + '/?s=' + encodeURIComponent(query);
         
-        console.log('[Dizibox V' + VER + '] Arama Sayfası: ' + searchUrl);
-        const sRes = await fetch(searchUrl);
+        console.log('[Dizibox V' + VER + '] 3. Arama Yapılıyor: ' + searchUrl);
+        
+        const sRes = await fetch(searchUrl, {
+            headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' }
+        });
         const html = await sRes.text();
 
-        // 3. Link Ayıklama
-        const linkMatch = findFirst(html, 'href="(https?:\\/\\/[^"]+dizibox[^"]+)"[^>]*rel="bookmark"');
-        
+        // Regex ile link ayıklama
+        const linkMatch = findFirst(html, 'href="(http[^"]+dizibox[^"]+)"[^>]*rel="bookmark"') || 
+                          findFirst(html, '<h2[^>]*>\\s*<a href="([^"]+)"');
+
         if (!linkMatch) {
-            console.log('[Dizibox V' + VER + '] HATA: Arama sonucunda link yakalanamadı.');
+            console.log('[Dizibox V' + VER + '] !!! HATA: Arama sonuçlarında link bulunamadı.');
             return [];
         }
 
-        const epUrl = linkMatch[1].replace('https:', 'http:') + '-sezon-' + seasonNum + '-bolum-' + episodeNum + '-izle/';
-        console.log('[Dizibox V' + VER + '] Son Durak: ' + epUrl);
+        // HTTPS'i HTTP'ye çevirerek emülatörün "Certificate Trust" hatasını atlıyoruz
+        const targetUrl = linkMatch[1].replace('https:', 'http:').replace(/\/$/, '') + 
+                          '-sezon-' + seasonNum + '-bolum-' + episodeNum + '-izle/';
+        
+        console.log('[Dizibox V' + VER + '] 4. Bölüm Sayfasına Gidiliyor: ' + targetUrl);
 
         return [{
             name: "⌜ Dizibox V" + VER + " ⌟",
-            url: epUrl,
+            url: targetUrl,
             quality: "1080p",
             headers: { 'Referer': BASE_URL }
         }];
 
-    } catch (e) {
-        console.log('[Dizibox V' + VER + '] KRİTİK HATA: ' + e.message);
+    } catch (err) {
+        console.log('[Dizibox V' + VER + '] !!! KRİTİK HATA: ' + err.message);
+        // Eğer hata TMDB'den kaynaklıysa, loglardan göreceğiz.
         return [];
     }
 }
 
-// Nuvio/Mooncrown için tam uyumlu export
+// Global Export
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = { getStreams: getStreams };
 } else {
