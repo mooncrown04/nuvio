d741230511513669afe27589773e169bfd996bb5
mooncrown04 <mooncrown04@gmail.com>
2026-03-10 20:53:37 +0300

    Update fullhd.js

--- eklentiler/fullhd.js
+++ eklentiler/fullhd.js
@@ -1,94 +1,158 @@
 /**
- * FullHDFilmizlesene Nuvio Scraper - v5.0 (Kotlin Uyumlu)
+ * FullHDFilmizlesene Nuvio Scraper - v5.1 (Syntax Fix)
  */
 
 var cheerio = require("cheerio-without-node-native");
 
-// Kotlin kodu ile AYNI base URL (www yok!)
-var BASE_URL = 'https://fullhdfilmizlesene.live';
+var BASE_URL = "https://fullhdfilmizlesene.live";
 
 var HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'tr-TR,tr;q=0.9',
-    'Referer': BASE_URL + '/'
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "tr-TR,tr;q=0.9",
+    "Referer": BASE_URL + "/"
 };
 
-function getStreams(tmdbId, mediaType, seasonNum, episodeNum) {
-    return new Promise(function(resolve, reject) {
-        
-        console.log('[FullHD] Starting:', {tmdbId, mediaType});
+function log(msg) {
+    console.log("[FullHD] " + msg);
+}
 
-        var tmdbType = (mediaType === 'movie' ? 'movie' : 'tv');
-        var tmdbUrl = 'https://api.themoviedb.org/3/' + tmdbType + '/' + tmdbId + 
-                      '?language=tr-TR&api_key=4ef0d7355d9ffb5151e987764708ce96';
+function getStreams(tmdbId, mediaType, seasonNum, episodeNum) {
+    return new Promise(function(resolve) {
+        
+        log("Starting: " + tmdbId + " type: " + mediaType);
+
+        var tmdbType = mediaType === "movie" ? "movie" : "tv";
+        var tmdbUrl = "https://api.themoviedb.org/3/" + tmdbType + "/" + tmdbId + 
+                      "?language=tr-TR&api_key=4ef0d7355d9ffb5151e987764708ce96";
 
         fetch(tmdbUrl)
             .then(function(res) { 
-                if (!res.ok) throw new Error('TMDB: ' + res.status);
+                if (!res.ok) throw new Error("TMDB: " + res.status);
                 return res.json(); 
             })
             .then(function(data) {
-                var query = data ? (data.title || data.name || data.original_title || data.original_name) : '';
-                if (!query) throw new Error('Isim bulunamadi');
+                var query = data ? (data.title || data.name || data.original_title || data.original_name) : "";
+                if (!query) throw new Error("Isim bulunamadi");
                 
-                console.log('[FullHD] Aranacak:', query);
+                log("Aranacak: " + query);
                 
-                // ✅ KRITIK: ?q= kullan (Kotlin ile aynı)
-                var searchUrl = BASE_URL + '/arama?q=' + encodeURIComponent(query).replace(/%20/g, '+');
-                console.log('[FullHD] Search URL:', searchUrl);
+                // Arama URL - Kotlin ile ayni
+                var searchUrl = BASE_URL + "/arama?q=" + encodeURIComponent(query).replace(/%20/g, "+");
+                log("Search: " + searchUrl);
                 
                 return fetch(searchUrl, { headers: HEADERS });
             })
             .then(function(res) { 
-                console.log('[FullHD] Arama status:', res.status);
-                if (!res.ok) throw new Error('Arama: ' + res.status);
+                if (!res.ok) throw new Error("Arama: " + res.status);
                 return res.text(); 
             })
             .then(function(html) {
-                console.log('[FullHD] HTML uzunluk:', html.length);
+                log("HTML uzunluk: " + html.length);
                 
                 var $ = cheerio.load(html);
                 
-                // Kotlin'deki gibi: .film-listesi .film-item
-                var firstResult = $('.film-listesi .film-item a').first().attr('href') ||
-                                  $('.film-liste .item a').first().attr('href') ||
-                                  $('a[href*="/film/"]').first().attr('href') ||
-                                  $('a[href*="/dizi/"]').first().attr('href');
+                // Link bul
+                var firstResult = null;
                 
-                console.log('[FullHD] Bulunan link:', firstResult);
+                // Once film ara
+                if (mediaType === "movie") {
+                    $("a[href*=\"/film/\"]").each(function(i, el) {
+                        if (!firstResult) {
+                            firstResult = $(el).attr("href");
+                        }
+                    });
+                } else {
+                    // Dizi ara
+                    $("a[href*=\"/dizi/\"]").each(function(i, el) {
+                        if (!firstResult) {
+                            firstResult = $(el).attr("href");
+                        }
+                    });
+                }
+                
+                // Genel ara
+                if (!firstResult) {
+                    $("a[href*=\"/film/\"], a[href*=\"/dizi/\"]").each(function(i, el) {
+                        if (!firstResult) {
+                            firstResult = $(el).attr("href");
+                        }
+                    });
+                }
+                
+                log("Bulunan link: " + firstResult);
                 
                 if (!firstResult) {
-                    // Debug için tüm linkleri göster
-                    var allLinks = [];
-                    $('a[href*="/film/"], a[href*="/dizi/"]').each(function(i, el) {
-                        allLinks.push($(el).attr('href'));
-                    });
-                    console.log('[FullHD] Tum linkler:', allLinks.slice(0, 10));
+                    log("Link bulunamadi");
                     return resolve([]);
                 }
 
-                // URL oluştur
+                // URL olustur
                 var targetUrl;
-                
-                if (firstResult.startsWith('http')) {
+                if (firstResult.indexOf("http") === 0) {
                     targetUrl = firstResult;
                 } else {
                     targetUrl = BASE_URL + firstResult;
                 }
                 
-                console.log('[FullHD] Hedef URL:', targetUrl);
+                log("Hedef: " + targetUrl);
                 
-                // Film mi dizi mi kontrol et
-                var isDizi = firstResult.includes('/dizi/');
-                
-                if (isDizi && mediaType === 'tv' && seasonNum && episodeNum) {
-                    // Dizi için bölüm URL'si oluştur
-                    // Kotlin kodundan: $mainUrl/dizi/$fixedTitle?season=$season&episode=$episode
-                    targetUrl = targetUrl + '?season=' + seasonNum + '&episode=' + episodeNum;
-                    console.log('[FullHD] Dizi URL:', targetUrl);
+                // Dizi icin sezon/episode ekle
+                if (mediaType === "tv" && seasonNum && episodeNum) {
+                    if (targetUrl.indexOf("?") === -1) {
+                        targetUrl = targetUrl + "?season=" + seasonNum + "&episode=" + episodeNum;
+                    } else {
+                        targetUrl = targetUrl + "&season=" + seasonNum + "&episode=" + episodeNum;
+                    }
+                    log("Dizi URL: " + targetUrl);
                 }
                 
                 return fetch(targetUrl, { headers: HEADERS });
             })
-           
+            .then(function(res) {
+                log("Icerik status: " + res.status);
+                if (!res.ok) throw new Error("Icerik: " + res.status);
+                return res.text();
+            })
+            .then(function(pageHtml) {
+                var $ = cheerio.load(pageHtml);
+                var streams = [];
+
+                $("iframe").each(function(i, elem) {
+                    var src = $(elem).attr("src") || $(elem).attr("data-src");
+                    if (src) {
+                        var url = src;
+                        if (url.indexOf("//") === 0) {
+                            url = "https:" + url;
+                        } else if (url.indexOf("http") !== 0) {
+                            url = "https:" + url;
+                        }
+                        
+                        log("Iframe " + i + ": " + url.substring(0, 80));
+                        
+                        streams.push({
+                            name: "FullHD Kaynak " + (i + 1),
+                            title: "FullHD",
+                            url: url,
+                            quality: "Auto",
+                            headers: HEADERS,
+                            provider: "fullhd"
+                        });
+                    }
+                });
+
+                log("Toplam stream: " + streams.length);
+                resolve(streams);
+            })
+            .catch(function(err) {
+                log("HATA: " + err.message);
+                resolve([]); 
+            });
+    });
+}
+
+if (typeof module !== "undefined" && module.exports) {
+    module.exports = { getStreams: getStreams };
+} else {
+    global.getStreams = getStreams;
+}
