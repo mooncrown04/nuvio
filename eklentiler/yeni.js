f91c02a87f87164f1ceb6e227a61c165c9f88375
mooncrown04 <mooncrown04@gmail.com>
2026-08-10 22:10:02 +0300

    Update remote URL in yeni.js

--- eklentiler/yeni.js
+++ eklentiler/yeni.js
@@ -1,6 +1,6 @@
 // filmmodu.js (Yerel dosyanın içeriği)
 async function loadRemoteProvider() {
- const remoteUrl = " https://raw.githubusercontent.com/mooncrown04/nuvio/refs/heads/master/eklentiler/dizix.js";
+ const remoteUrl = "https://raw.githubusercontent.com/mooncrown04/nuvio/refs/heads/master/eklentiler/m3u.js";
     //  const remoteUrl = "https://raw.githubusercontent.com/hihihihihiiray/plugins/refs/heads/main/providers/filmmodu.js";
     try {
         const response = await fetch(remoteUrl);
